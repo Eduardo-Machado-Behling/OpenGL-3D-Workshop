46b0d06afa46abffc2234ed6cec41556126bde46
Thales Rodrigues <thaleshend@gmail.com>
2025-08-04 22:36:50 -0300

    Checkpoint #3: setup

---
+++ src/utils.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+std::string ReadFile(const std::string& filePath);
+
+unsigned int LoadTexture(const std::string& texture_path);
+
+// bool LoadObj(const std::string& obj_file_path, std::vector<Mesh::Vertex>& vertices, std::vector<unsigned int>& indices);
